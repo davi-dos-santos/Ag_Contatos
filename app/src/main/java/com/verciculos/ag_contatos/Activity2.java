479b1a6eab6ee99259284dc4881121dc0b9f91cf
davi1701 <davi.spereira1@gmial.com>
2022-05-22 00:24:04 -0300

    transições de telas

--- app/src/main/java/com/verciculos/ag_contatos/Activity2.java
+++ app/src/main/java/com/verciculos/ag_contatos/Activity2.java
@@ -6,21 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Activity2 extends AppCompatActivity {
 
-    Button Btn_Voltar;
+    ImageButton Btn_Voltar_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        Btn_Voltar=(Button) findViewById(R.id.Btn_Voltar);
+        Btn_Voltar_img=(ImageButton) findViewById(R.id.Btn_Voltar_img);
     }
 
     public void Voltar_actvity_main(View v) {
-        Intent voltar = new Intent(this, activity2.class);
+        Intent voltar = new Intent(this, MainActivity.class);
         startActivity(voltar);
     }
 }
--- app/src/main/java/com/verciculos/ag_contatos/MainActivity.java
+++ app/src/main/java/com/verciculos/ag_contatos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.verciculos.ag_contatos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button Btn_Cadastrar, Btn_listar;
+
+    SQLiteDatabase db=null;
 
 
     @Override
@@ -20,10 +27,40 @@ public class MainActivity extends AppCompatActivity {
 
         Btn_Cadastrar=(Button) findViewById(R.id.Btn_Cadastrar);
         Btn_listar=(Button) findViewById(R.id.Btn_listar);
+
+        criarAbrirDB();
+        fecharDB();
+
+    }
+
+    public void criarAbrirDB(){
+        try {
+            db=openOrCreateDatabase("BancoAgenda", MODE_PRIVATE, null);
+        }catch (Exception ex){
+            msg("Erro ao abrir ou criar BD");
+        }
+        try {
+            db.execSQL("CREATE TABLE IF NOT EXISTS contatos (id INTEGER PRIMARY KEY, nome TEXT, fone TEXT, email TEXT, endereço TEXT, dataNsc TEXT)");
+        }catch (Exception ex){
+            msg("Erro ao abrir ou criar Tabela");
+        }finally {
+            msg("Tabela contatos criada");
+        }
+    }
+
+    public void fecharDB(){
+        db.close();
     }
 
     public void abrir_Actvity2(View v){
-        Intent listar=new Intent(this, activity2.class);
+        Intent listar=new Intent(this, Activity2.class);
         startActivity(listar);
+    }
+
+    public void msg(String txt){
+        AlertDialog.Builder adb=new AlertDialog.Builder(this);
+        adb.setMessage(txt);
+        adb.setNeutralButton("OK", null);
+        adb.show();
     }
 }
